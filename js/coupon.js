5e3937c578b8f277086df66fa55117956c281f4a
oreopot <bhaumikmehta2017@gmail.com>
2022-03-17 19:42:02 +1100

    initial commit for fetch lectures

--- js/coupon.js
+++
@@ -1,3 +0,0 @@
-export const basicCoupon = () => 'FirstCustomer'
-export const basicCoupon1 = () => 'response from first function'
-export const basicCoupon2 = () => 'response from second function'
--- js/index.js
+++
@@ -1,8 +0,0 @@
-import PI, { squareOfNumber as square123, cubeOfNumber as cube } from './utils.js'
-import basicDiscount, {basicCoupon} from './discount.js'
-
-console.log(basicDiscount())
-console.log(basicCoupon())
-console.log(`Constant PI: ${PI}`);
-console.log(cube(5))
-console.log(square123(5))
